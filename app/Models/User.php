c3938f6561457968c8e0c2dfcace008a2d963248
yaojinhui <yaojinhui1993@gmail.com>
2020-12-02 13:13:54 +0800

    确定邮件验证的功能

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable, MustVerifyEmailTrait;
 
     /**
      * The attributes that are mass assignable.
