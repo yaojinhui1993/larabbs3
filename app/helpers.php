abf43bd82ac5b19f8de0c682612473520217bf11
yaojinhui <yaojinhui1993@gmail.com>
2020-12-04 18:06:09 +0800

    User can create topics

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Route;
 
 function route_class()
@@ -12,4 +13,11 @@ function category_nav_active($categoryId)
     return active_class(
         (if_route('categories.show') && if_route_param('category', $categoryId))
     );
+}
+
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+
+    return Str::limit($excerpt, $length);
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -45,6 +45,9 @@
                         <a href="{{ route('register') }}" class="nav-link">注册</a>
                     </li>
                 @else
+                    <li class="nav-item">
+                        <a href="{{ route('topics.create') }}" class="nav-link mt-1 mr-3 font-weight-bold"><i class="fa fa-plus"></i></a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
