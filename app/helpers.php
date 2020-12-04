b02ee98671fa71e22ccdc108cc0a10a60b2406db
yaojinhui <yaojinhui1993@gmail.com>
2020-12-04 13:22:32 +0800

    Category topic list.

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,10 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function category_nav_active($categoryId)
+{
+    return active_class(
+        (if_route('categories.show') && if_route_param('category', $categoryId))
+    );
+}
