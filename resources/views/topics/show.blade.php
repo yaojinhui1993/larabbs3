8451bc779bb7fdb08890afb41015c292c78abaca
yaojinhui <yaojinhui1993@gmail.com>
2020-12-10 16:07:53 +0800

    Add topic destroy policy

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -42,16 +42,18 @@
                     {!! $topic->body !!}
                 </div>
 
-                <div class="operate">
-                    <hr>
-                    <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
-                        <i class="far fa-edit"></i>编辑
-                    </a>
+                @can('update', $topic)
+                    <div class="operate">
+                        <hr>
+                        <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
+                            <i class="far fa-edit"></i>编辑
+                        </a>
 
-                    <a href="#" class="btn btn-outline-secondary btn-sm" role="button">
-                        <i class="far fa-edit"></i>删除
-                    </a>
-                </div>
+                        <a href="#" class="btn btn-outline-secondary btn-sm" role="button">
+                            <i class="far fa-edit"></i>删除
+                        </a>
+                    </div>
+                @endcan
             </div>
         </div>
     </div>
