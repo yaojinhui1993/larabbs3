d5138c4a435ad71084d5f00222fc825872da5b1a
yaojinhui <yaojinhui1993@gmail.com>
2020-12-11 09:32:53 +0800

    Translate topic title slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Handlers\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -14,5 +15,10 @@ class TopicObserver
         $topic->body = clean($topic->body, 'user_topic_body');
 
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如果 slug 无内容，则使用翻译器对 title 进行翻译
+        if (! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -10,10 +10,10 @@
 
                 <div class="media-body">
                     <div class="media-heading mt-0 mb-1">
-                        <a href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
+                        <a href="{{ $topic->link() }}" title="{{ $topic->title }}">
                             {{ $topic->title }}
                         </a>
-                        <a href="{{ route('topics.show', [$topic->id]) }}" class="float-right">
+                        <a href="{{ $topic->link() }}" class="float-right">
                             <span class="badge badge-secondary badge-pill">{{ $topic->reply_count }}</span>
                         </a>
                     </div>
