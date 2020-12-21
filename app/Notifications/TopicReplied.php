ccc7647c1d7f6854e55150185e774125b6571634
yaojinhui <yaojinhui1993@gmail.com>
2020-12-21 16:35:50 +0800

    Support mail channel.

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -5,8 +5,9 @@ namespace App\Notifications;
 use App\Models\Reply;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements Queueable
 {
     use Queueable;
 
@@ -31,7 +32,7 @@ class TopicReplied extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
@@ -56,5 +57,14 @@ class TopicReplied extends Notification
             'topic_id' => $topic->id,
             'topic_title' => $topic->title,
         ];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+            ->line('你的话题有新回复')
+            ->action('查看回复', $url);
     }
 }
