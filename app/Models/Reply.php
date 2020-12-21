77c91acc409003ec0bf041f2f9f3df5fab8c4e34
yaojinhui <yaojinhui1993@gmail.com>
2020-12-21 13:07:09 +0800

    User can reply topics.

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -15,4 +15,6 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    
 }
---
+++ resources/views/topics/_reply_box.blade.php
@@ -0,0 +1,16 @@
+@include('shared._error')
+
+<div class="reply-box">
+    <form action="{{ route('replies.store') }}" method="post" accept-charset="UTF-8">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="hidden" name="topic_id" value="{{ $topic->id }}">
+
+        <div class="form-group">
+            <textarea name="content" rows="3" class="form-control" placeholder="分享你的见解~"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary btn-sm">
+            <i class="fa fa-share mr-1"></i>回复
+        </button>
+    </form>
+</div>
