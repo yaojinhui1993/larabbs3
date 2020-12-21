a30a20927842baad3aa365686009ee64d2d9c116
yaojinhui <yaojinhui1993@gmail.com>
2020-12-21 13:21:18 +0800

    Increment user notification count when it has new notification

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,7 +10,12 @@ use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable, MustVerifyEmailTrait;
+    use MustVerifyEmailTrait;
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+
 
     /**
      * The attributes that are mass assignable.
@@ -51,5 +57,20 @@ class User extends Authenticatable implements MustVerifyEmail
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了
+        if ($this->id === Auth::id()) {
+            return;
+        }
+
+        // 只有数据库类型通知才需要提醒
+        if (method_exists($instance, 'toDatabase')) {
+            $this->increment('notification_count');
+        }
+
+        $this->laravelNotify($instance);
     }
 }
