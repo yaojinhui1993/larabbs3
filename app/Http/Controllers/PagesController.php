f2ef1e8dfa7ab65e917dd121ea2dd6ccd44d5f0c
yaojinhui <yaojinhui1993@gmail.com>
2020-12-24 17:54:39 +0800

    manage backend permissions

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,4 +8,13 @@ class PagesController extends Controller
     {
         return view('pages.root');
     }
+
+    public function permissionDenied()
+    {
+        if (config('administrator.permission')()) {
+            return redirect(config('administrator.uri'), 302);
+        }
+
+        return view('pages.permission_denied');
+    }
 }
---
+++ resources/views/pages/permission_denied.blade.php
@@ -0,0 +1,25 @@
+@extends('layotus.app')
+
+@section('title', '无权限访问')
+
+@section('content')
+    <div class="col-md-4 offset-md-4">
+        <div class="card">
+            <div class="card-body">
+                @if(Auth::check())
+                    <div class="alert alert-danger text-center mb-0">
+                        当前登录帐号无后台访问权限。
+                    </div>
+                @else
+                    <div class="alert alert-danger text-center">
+                        请登录以后再操作
+                    </div>
+
+                    <a href="{{ route('login') }}" class="btn btn-lg btn-primary btn-block">
+                        <i class="fas fa-sign-in-alt"></i>登录
+                    </a>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
