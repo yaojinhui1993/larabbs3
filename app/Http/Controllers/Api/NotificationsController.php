90d4559c0193a5de3007e0235ff86f0b292327d6
yaojinhui <yaojinhui1993@gmail.com>
2020-12-30 09:29:42 +0800

    notifications index

---
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\NotificationResource;
+
+class NotificationsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $notifications = $request->user()->notifications()->paginate();
+
+        return NotificationResource::collection($notifications);
+    }
+}
