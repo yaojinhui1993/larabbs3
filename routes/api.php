b5ece99e196bd3282509a71b3d11fb8eda3cdf16
yaojinhui <yaojinhui1993@gmail.com>
2020-12-30 09:44:13 +0800

    User permissions

--- routes/api.php
+++ routes/api.php
@@ -71,6 +71,8 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
             Route::get('notifications', 'NotificationsController@index')->name('notifications.index'); // 通知列表
             Route::get('notifications/stats', 'NotificationsController@stats')->name('notifications.stats'); // 通知统计
             Route::patch('user/read/notifications', 'NotificationsController@read')->name('user.notifications.read'); // 标记消息通知已读
+
+            Route::get('user/permissions', 'PermissionsController@index')->name('user.permissions.index'); // 当前登录用户的权限
         });
     });
 });
